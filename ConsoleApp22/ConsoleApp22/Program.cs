69d5281ed3dfca7ef58da4839bfaadb32e489327
Vi-pinV <164785228+Vi-pinV@users.noreply.github.com>
2024-03-25 01:39:20 +0530

    Add files via upload

---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        
+        enum UserChoice
+        {
+            AddTask = 1, ListTask = 2, Exit=3
+        }
+        static void Main(string[] args)
+        {
+            List<string> toDoList = new List<string>();
+            while (true)
+            {
+                
+                Console.WriteLine("1. AddTask");
+                Console.WriteLine("2. Exit");
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == (int)UserChoice.AddTask) {
+                    Console.WriteLine("Enter task:");
+                    string task = Console.ReadLine();
+                    toDoList.Add(task);
+                    Console.Clear();
+                    Console.WriteLine("Task added succesfully");
+                }
+                else if(choice == (int)UserChoice.ListTask)
+                {
+                    if (toDoList.Count > 0)
+                    {
+                        Console.WriteLine("TO-Do-List");
+                        for (int i = 0; i < toDoList.Count; i++)
+                        {
+                            Console.WriteLine("-" + toDoList[i]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Empty list");
+                    }
+                }
+                else if (choice == (int)UserChoice.Exit)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
+
+
